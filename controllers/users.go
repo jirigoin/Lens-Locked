d9e134b186cad5b43534c9cd547534e7a1b4ca8a
Juan Irigoin <juan.irigoin@mercadolibre.com>
2021-12-15 18:17:01 -0300

    add controller pkg

---
+++ controllers/users.go
@@ -0,0 +1,24 @@
+package controllers
+
+import (
+	"net/http"
+
+	"lenslocked/views"
+)
+
+// NewUsers is used to create a new Users controller.
+func NewUsers() *Users {
+	return &Users{
+		NewView: views.NewView("bootstrap", "views/users/new.gohtml"),
+	}
+}
+
+type Users struct {
+	NewView *views.View
+}
+
+func (u *Users) New(w http.ResponseWriter, r *http.Request) {
+	if err := u.NewView.Render(w, nil); err != nil {
+		panic(err)
+	}
+}
